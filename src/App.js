efdb83a4eeb81d1e9267feb3aa6cabf7f7f10c7b
leeheeae <chromeee@daum.net>
2022-04-06 23:19:39 +0900

    페이지 생성 및 react-dom 업데이트 수정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import WritePage from './pages/WritePage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<PostListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        {/* <Route path={['/@:username', '/*']} element={<PostListPage />} /> */}
+        <Route path="/@:username/:postId" element={<PostPage />} />
+        <Route path="/write" element={<WritePage />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
