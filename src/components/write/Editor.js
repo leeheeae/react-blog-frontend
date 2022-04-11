e279fe37dabe3c5fe4d347c977f73d124708be9b
leeheeae <chromeee@daum.net>
2022-04-11 16:43:44 +0900

    write 페이지 리듀서

--- src/components/write/Editor.js
+++ src/components/write/Editor.js
@@ -30,7 +30,7 @@ const QuillWrapper = styled.div`
   }
 `;
 
-const Editor = () => {
+const Editor = ({ onChangeField, title, body }) => {
   const quillElement = useRef(null); //Quill을 적용할 DivElement를 설정
   const quillInstance = useRef(null); //Quill 인스턴스를 설정
 
@@ -43,15 +43,31 @@ const Editor = () => {
           [{ header: '1' }, { header: '2' }],
           ['bold', 'italic', 'underline', 'strike'],
           [{ list: 'ordered' }, { list: 'bullet' }],
-          ['blockquete', 'code-block', 'link', 'image'],
+          ['blockquote', 'code-block', 'link', 'image'],
         ],
       },
     });
-  }, []);
+
+    //quill에 text-change 이벤트 핸들러 등록
+    const quill = quillInstance.current;
+    quill.on('text-change', (delta, onDelta, source) => {
+      if (source === 'user') {
+        onChangeField({ key: 'body', value: quill.root.innerHTML });
+      }
+    });
+  }, [onChangeField]);
+
+  const onChangeTitle = (e) => {
+    onChangeField({ key: 'title', value: e.target.value });
+  };
 
   return (
     <EditorBlock>
-      <TitleInput placeholder="제목을 입력하세요" />
+      <TitleInput
+        placeholder="제목을 입력하세요"
+        onChange={onChangeTitle}
+        value={title}
+      />
       <QuillWrapper>
         <div ref={quillElement} />
       </QuillWrapper>
