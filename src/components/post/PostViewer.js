50049c15a9716cf37d8f10ea6987a517a7c55fd9
leeheeae <chromeee@daum.net>
2022-04-11 19:55:05 +0900

    listpage

---
+++ src/components/post/PostViewer.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+
+const PostViewerBlock = styled(Responsive)`
+  margin-top: 4rem;
+`;
+const PostHead = styled.div`
+  border-bottom: 1px solid ${palette.gray[2]};
+  padding-bottom: 3rem;
+  margin-bottom: 3rem;
+  h1 {
+    font-size: 3rem;
+    line-height: 1.5;
+    margin: 0;
+  }
+`;
+
+const SubInfo = styled.div`
+  margin-top: 1rem;
+  color: ${palette.gray[6]};
+
+  span + span::before {
+    color: ${palette.gray[5]};
+    padding: 0 0.25rem;
+    content: '\\B7';
+  }
+`;
+
+const Tags = styled.div`
+  margin-top: 0.5rem;
+  .tag {
+    display: inline-block;
+    color: ${palette.violet[7]};
+    text-decoration: none;
+    margin-right: 0.5rem;
+
+    &:hover {
+      color: ${palette.violet[6]};
+    }
+  }
+`;
+
+const PostContent = styled.div`
+  font-size: 1.3125rem;
+  color: ${palette.gray[8]};
+`;
+
+const PostViewer = ({ post, loading, error }) => {
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewerBlock>존재하기 않는 포스트입니다.</PostViewerBlock>;
+    }
+    return <PostViewerBlock>오류 발생</PostViewerBlock>;
+  }
+
+  //로딩중이거나 아직 포스트 데이터가 없을 떄
+  if (loading || !post) {
+    return null;
+  }
+
+  const { title, body, user, publishedDate, tags } = post;
+
+  return (
+    <PostViewerBlock>
+      <PostHead>
+        <h1>{title}</h1>
+        <SubInfo>
+          <span>
+            <b>{user.username}</b>
+          </span>
+          <span>{new Date(publishedDate).toLocaleDateString()}</span>
+        </SubInfo>
+        <Tags>
+          {tags.map((tag) => (
+            <div className="tag" key={tag}>
+              #{tag}
+            </div>
+          ))}
+        </Tags>
+      </PostHead>
+      <PostContent dangerouslySetInnerHTML={{ __html: body }} />
+    </PostViewerBlock>
+  );
+};
+
+export default PostViewer;
---
+++ src/containers/post/PostViewerContainer.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { readPost, unloadPost } from '../../modules/post';
+import PostViewer from '../../components/post/PostViewer';
+
+const PostViewerContainer = () => {
+  const { postId } = useParams();
+  const dispatch = useDispatch();
+  const { post, error, loading } = useSelector(({ post, loading }) => ({
+    post: post.post,
+    error: post.error,
+    loading: loading['post/READ_POST'],
+  }));
+
+  useEffect(() => {
+    dispatch(readPost(postId));
+
+    //언마운트 될 때 리덕스에서 포스트 데이터 없애기
+    return () => {
+      dispatch(unloadPost());
+    };
+  }, [dispatch, postId]);
+
+  return <PostViewer post={post} loading={loading} error={error} />;
+};
+
+export default PostViewerContainer;
